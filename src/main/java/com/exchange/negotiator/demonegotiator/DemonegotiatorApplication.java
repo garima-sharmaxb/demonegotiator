2a49f238a2cb399d1f39272cc1df027cf76411c6
Siddharth Kumar <siddharthkumar@XEBIAINDIA.LOCAL>
2018-02-08 13:03:14 +0530

    negotiator project intitial setup

---
+++ src/main/java/com/exchange/negotiator/demonegotiator/DemonegotiatorApplication.java
@@ -0,0 +1,12 @@
+package com.exchange.negotiator.demonegotiator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonegotiatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonegotiatorApplication.class, args);
+	}
+}
